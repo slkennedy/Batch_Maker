06faadb47c99dbd749b620536654ef4bf5266fd8
slkennedy <slkennedy@gmail.com>
2014-10-14 22:35:33 -0400

    Inital Commit. Creates Directory. Creates Application and User Template

---
+++ app/scripts/router.js
@@ -0,0 +1,25 @@
+App.Router.map (function(){
+	this.route('user' /*,{path:':user_name'}*/);
+
+	//make a user template
+	//make UserRoute
+	//hook model to UserRoute
+
+	this.resource('recipes', function(){
+		this.route('create');
+	// 	this.route('show', {path: ':recipe_id'});
+	// 	this.route('edit', {path: ':recipe_id/edit'});
+	});
+
+	// this.route('login');
+	// this.route('signup');
+
+	// this.route('settings');
+	// this.route('pantry');
+});
+
+App.UserRoute = Ember.Route.extend ({
+	model: function (){
+		return {username: 'Sara K'};
+	}
+});
